12d0de8a1316bed62ecd16fa66a0ed16a0103358
AhmadAlkhen <103906603+AhmadAlkhen@users.noreply.github.com>
2023-11-07 01:13:49 +0400

    Update 2014_10_12_000000_create_users_table.php
    add default user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\Hash;
 return new class extends Migration
 {
     /**
@@ -20,6 +20,12 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+               // Add a default user
+               \DB::table('users')->insert([
+                'name' => 'User',
+                'email' => 'user@user.com',
+                'password' => Hash::make('user'),
+            ]);
     }
 
     /**
